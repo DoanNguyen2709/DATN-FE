593bc33c1ef2429241e5753061400144356d0719
DoanNguyen2709 <dn938901ahigi@gmail.com>
2024-01-24 21:37:57 +0700

    FE

---
+++ store/actions/adminActions.js
@@ -0,0 +1,236 @@
+import actionTypes from "./actionTypes";
+import {
+  getAllCodeService,
+  createNewUserService,
+  getAllUsers,
+  deleteUserService,
+  editUserService,
+  getTopDoctorHomeService,
+} from "../../services/userService";
+import { toast } from "react-toastify";
+
+// export const fetchGenderStart = () => ({
+//   type: actionTypes.FETCH_GENDER_START,
+// });
+
+export const fetchGenderStart = (dispatch, getState) => {
+  return async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: actionTypes.FETCH_GENDER_START,
+      });
+      let res = await getAllCodeService("GENDER");
+      if (res && res.errCode === 0) {
+        dispatch(fetchGenderSuccess(res.data));
+      } else {
+        dispatch(fetchGenderFailed());
+      }
+    } catch (e) {
+      dispatch(fetchGenderFailed());
+      console.log("fetchGenderStart error", e);
+    }
+  };
+};
+
+export const fetchGenderSuccess = (genderData) => ({
+  type: actionTypes.FETCH_GENDER_SUCCESS,
+  data: genderData,
+});
+
+export const fetchGenderFailed = () => ({
+  type: actionTypes.FETCH_GENDER_FAILED,
+});
+
+export const fetchPositionSuccess = (positionData) => ({
+  type: actionTypes.FETCH_POSITION_SUCCESS,
+  data: positionData,
+});
+
+export const fetchPositionFailed = () => ({
+  type: actionTypes.FETCH_POSITION_FAILED,
+});
+
+export const fetchRoleSuccess = (roleData) => ({
+  type: actionTypes.FETCH_ROLE_SUCCESS,
+  data: roleData,
+});
+
+export const fetchRoleFailed = () => ({
+  type: actionTypes.FETCH_ROLE_FAILED,
+});
+
+export const fetchPositionStart = (dispatch, getState) => {
+  return async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: actionTypes.FETCH_GENDER_START,
+      });
+      let res = await getAllCodeService("POSITION");
+      if (res && res.errCode === 0) {
+        dispatch(fetchPositionSuccess(res.data));
+      } else {
+        dispatch(fetchPositionFailed());
+      }
+    } catch (e) {
+      dispatch(fetchPositionFailed());
+      console.log("fetchPositionStart error", e);
+    }
+  };
+};
+
+export const fetchRoleStart = () => {
+  return async (dispatch, getState) => {
+    try {
+      let res = await getAllCodeService("ROLE");
+      if (res && res.errCode === 0) {
+        dispatch(fetchRoleSuccess(res.data));
+      } else {
+        dispatch(fetchRoleFailed());
+      }
+    } catch (e) {
+      dispatch(fetchRoleFailed());
+      console.log("fetchRoleStart error", e);
+    }
+  };
+};
+
+export const createNewUser = (data) => {
+  return async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: actionTypes.FETCH_GENDER_START,
+      });
+      let res = await createNewUserService(data);
+      if (res && res.errCode === 0) {
+        toast.success("Create a new user success");
+        dispatch(saveUserSuccess());
+        dispatch(fetchAllUserStart());
+      } else {
+        dispatch(saveUserFailed());
+      }
+    } catch (e) {
+      dispatch(saveUserFailed());
+      console.log("saveUserFailed error", e);
+    }
+  };
+};
+
+export const saveUserSuccess = () => ({
+  type: actionTypes.CREATE_USER_SUCCESS,
+});
+
+export const saveUserFailed = () => ({
+  type: actionTypes.CREATE_USER_FAILED,
+});
+
+export const fetchAllUserStart = () => {
+  return async (dispatch, getState) => {
+    try {
+      let res = await getAllUsers("ALL");
+      if (res && res.errCode === 0) {
+        dispatch(fetchAllUserSuccess(res.users.reverse()));
+      } else {
+        toast.error("Fetch all user error");
+
+        dispatch(fetchAllUserFailed());
+      }
+    } catch (e) {
+      toast.error("Fetch all user error");
+
+      dispatch(fetchAllUserFailed());
+      console.log("fetchAllUserFailed error", e);
+    }
+  };
+};
+
+export const fetchAllUserSuccess = (data) => ({
+  type: actionTypes.FETCH_ALL_USER_SUCCESS,
+  users: data,
+});
+
+export const fetchAllUserFailed = (data) => ({
+  type: actionTypes.FETCH_ALL_USER_FAILED,
+});
+
+export const deleteAUser = (userId) => {
+  return async (dispatch, getState) => {
+    try {
+      let res = await deleteUserService(userId);
+      if (res && res.errCode === 0) {
+        toast.success("Delete user success");
+        dispatch(deleteUserSuccess());
+        dispatch(fetchAllUserStart());
+      } else {
+        toast.error("Delete user error");
+        dispatch(deleteUserFailed());
+      }
+    } catch (e) {
+      toast.error("Delete user error");
+
+      dispatch(deleteUserFailed());
+      console.log("deleteUserFailed error", e);
+    }
+  };
+};
+
+export const deleteUserSuccess = () => ({
+  type: actionTypes.DELETE_USER_SUCCESS,
+});
+
+export const deleteUserFailed = () => ({
+  type: actionTypes.DELETE_USER_FAILED,
+});
+
+export const editAUser = (data) => {
+  return async (dispatch, getState) => {
+    try {
+      let res = await editUserService(data);
+      if (res && res.errCode === 0) {
+        toast.success("Update user success");
+        dispatch(editUserSuccess());
+        dispatch(fetchAllUserStart());
+      } else {
+        toast.error("Update user error");
+        dispatch(editUserFailed());
+      }
+    } catch (e) {
+      toast.error("Update user error");
+
+      dispatch(editUserFailed());
+      console.log("editUserFailed error", e);
+    }
+  };
+};
+
+export const editUserSuccess = () => ({
+  type: actionTypes.EDIT_USER_SUCCESS,
+});
+
+export const editUserFailed = () => ({
+  type: actionTypes.EDIT_USER_FAILED,
+});
+
+// let res1 = await getTopDoctorHomeService(3);
+
+export const fetchTopDoctor = () => {
+  return async (dispatch, getState) => {
+    try {
+      let res = await getTopDoctorHomeService("");
+      if (res && res.errCode === 0) {
+        dispatch({
+          type: actionTypes.FETCH_TOP_DOCTORS_SUCCESS,
+          dataDoctors: res.data,
+        });
+      } else {
+        dispatch({
+          type: actionTypes.FETCH_TOP_DOCTORS_FAILED,
+        });
+      }
+    } catch (e) {
+      console.log("FETCH_TOP_DOCTOR_FAILED: ", e);
+      dispatch({
+        type: actionTypes.FETCH_TOP_DOCTORS_FAILED,
+      });
+    }
+  };
+};
---
+++ store/reducers/adminReducer.js
@@ -0,0 +1,97 @@
+import actionTypes from "../actions/actionTypes";
+
+const initialState = {
+  isLoadingGender: false,
+  genders: [],
+  roles: [],
+  positions: [],
+  users: [],
+  topDoctors: [],
+};
+
+const adminReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.FETCH_GENDER_START:
+      let copyState = { ...state };
+      copyState.isLoadingGender = true;
+
+      return {
+        ...copyState,
+      };
+
+    case actionTypes.FETCH_GENDER_SUCCESS:
+      state.genders = action.data;
+      state.isLoadingGender = false;
+
+      return {
+        ...state,
+      };
+
+    case actionTypes.FETCH_GENDER_FAILED:
+      state.genders = [];
+      state.isLoadingGender = false;
+
+      return {
+        ...state,
+      };
+
+    case actionTypes.FETCH_POSITION_SUCCESS:
+      state.positions = action.data;
+
+      return {
+        ...state,
+      };
+
+    case actionTypes.FETCH_POSITION_FAILED:
+      state.positions = [];
+
+      return {
+        ...state,
+      };
+
+    case actionTypes.FETCH_ROLE_SUCCESS:
+      state.roles = action.data;
+
+      return {
+        ...state,
+      };
+
+    case actionTypes.FETCH_ROLE_FAILED:
+      state.roles = [];
+
+      return {
+        ...state,
+      };
+
+    case actionTypes.FETCH_ALL_USER_SUCCESS:
+      state.users = action.users;
+
+      return {
+        ...state,
+      };
+
+    case actionTypes.FETCH_ALL_USER_FAILED:
+      state.users = [];
+
+      return {
+        ...state,
+      };
+
+    case actionTypes.FETCH_TOP_DOCTORS_SUCCESS:
+      state.topDoctors = action.dataDoctors;
+
+      return {
+        ...state,
+      };
+
+    case actionTypes.FETCH_TOP_DOCTORS_FAILED:
+      state.topDoctors = [];
+
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  }
+};
+export default adminReducer;
